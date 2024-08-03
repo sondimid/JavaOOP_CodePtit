588c9f8e1d5bae217c014c1ff5f58797169ea349
sondimid <daongocson12022004@gmail.com>
2024-08-03 18:50:53 +0700

    update

--- Thu Nhap Giao Vien/Main.java
+++ Thu Nhap Giao Vien/Main.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String []arg){
         Scanner sc = new Scanner(System.in);
+        GV a = new GV(sc.nextLine(),sc.nextLine(),sc.nextLong());
+        a.salary();
+        System.out.println(a.getMa()+" "+a.getTen()+" "+a.getHeSo()+" "+a.getPhuCap()+" " +a.getSalary());
     }
 }

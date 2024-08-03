b8e88f7ab4ce0fcb969cf5b10f6416118159b98d
sondimid <daongocson12022004@gmail.com>
2024-08-04 00:21:08 +0700

    update

---
+++ Lop Tam Giac/Triangle.java
@@ -0,0 +1,19 @@
+public class Triangle {
+    private double a,b,c,perimeter;
+
+    public Triangle(Point A,Point B, Point C) {
+        this.a = B.distance(C);
+        this.b = A.distance(C);
+        this.c = B.distance(A);
+        this.perimeter = a+b+c;
+    }
+    public boolean valid(){
+        if(this.a+this.b<=this.c || this.c+this.b <= this.a || this.a+this.c <=this.b) return false;
+        return true;
+    }
+
+    public double getPerimeter() {
+        double roundedPerimeter = Math.round(this.perimeter *1000.0)/1000.0;
+        return roundedPerimeter;
+    }
+}

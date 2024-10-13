eaa667f8962aa12023bc42505a0b6effe3bf7132
sondimid <daongocson12022004@gmail.com>
2024-10-13 16:19:56 +0700

    new

--- Thuc hanh 13t10/soKhacNhauTrongFileNhiPhan/soKhacNhauTrongFileNhiPhan.java
+++ Thuc hanh 13t10/soKhacNhauTrongFileNhiPhan/soKhacNhauTrongFileNhiPhan.java
@@ -1,4 +1,12 @@
 package soKhacNhauTrongFileNhiPhan;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class soKhacNhauTrongFileNhiPhan {
+    public static void main(String[] args) throws IOException {
+        ObjectInputStream file = new ObjectInputStream(new FileInputStream("DATA.IN"));
+    }
 }
